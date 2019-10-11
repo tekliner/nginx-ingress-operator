3407e73d7f0e8ef89bfedc19b8a9b9b344ad37cb
Yaroslav Shishnev <defender13@gmail.com>
2019-10-11 18:26:57 +0700

    test maxU

--- pkg/controller/nginxingress/nginxingress_controller.go
+++ pkg/controller/nginxingress/nginxingress_controller.go
@@ -347,6 +347,7 @@ func (r *ReconcileNginxIngress) Reconcile(request reconcile.Request) (reconcile.
 	}
 
 	// reconcile podDisruptionBudget
+	reqLogger.Info("Pdb ")
 	if instance.Spec.DefaultBackend != nil {
 		replicas := int32(1)
 		reqLogger.Info("Backend")
